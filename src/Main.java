3e8ae25c2ede13c3dea51e189adc21d695638009
SvetlanaShin <susperson@gmail.com>
2022-08-24 18:51:46 +0600

    Переменные ч.1 ДЗ2. Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 2_4
+        var friend = 19;
+        System.out.println(friend);
 
+        friend = friend * 2;
+        System.out.println(friend);
+
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
