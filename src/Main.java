bb2ba6facd4d2c8897d322e0beb8bcba2e1bed69
SvetlanaShin <susperson@gmail.com>
2022-08-24 18:54:43 +0600

    Переменные ч.1 ДЗ2. Задание 4-5

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
 
         friend = friend / 7;
         System.out.println(friend);
+
+        //Задание 2_5
+        var frog = 3.5;
+        System.out.println(frog);
+
+        frog = frog * 10;
+        System.out.println(frog);
+
+        frog = frog / 3.5;
+        System.out.println(frog);
+
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
