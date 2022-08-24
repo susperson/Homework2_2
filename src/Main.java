9eb2997309b260c6387d78bf131b8c6d37932097
SvetlanaShin <susperson@gmail.com>
2022-08-24 19:52:33 +0600

    Переменные ч.1 ДЗ2. Задания 4-5 после самопроверки

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //Задание 2_4
+        System.out.println("Задача 4");
         var friend = 19;
         System.out.println(friend);
 
-        friend = friend * 2;
+        friend = friend + 2;
         System.out.println(friend);
 
         friend = friend / 7;
         System.out.println(friend);
 
         //Задание 2_5
+        System.out.println("Задача 5");
         var frog = 3.5;
         System.out.println(frog);
 
